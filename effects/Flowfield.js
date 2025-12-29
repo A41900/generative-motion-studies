0d347a41498ee044ce9fa91e4a9cc45333b13d29
Beatriz <A41900@alunos.isel.pt>
2025-12-29 00:03:09 +0000

    style: remove unused comments

--- effects/Flowfield.js
+++ effects/Flowfield.js
@@ -2,9 +2,7 @@
 // Effect de DEBUG: desenha uma grid de vetores
 // onde a direção segue uma "corrente" curva global
 
-import { applyFieldAsDisplacement, softAttract } from "../motion/visuals.js";
 import { Particle } from "../core/Particle.js";
-import { radialField } from "../fields/fields.js";
 import { createNoise3D } from "../fields/simplex-noise.js";
 
 export class FlowField {
